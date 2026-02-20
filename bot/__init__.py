54d41d21e2dc1bf2f3e0768a9913fda0ce54ae39
Antonin <antonin.chenavaz@gmail.com>
2026-02-20 22:14:29 +0100

    Create __init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,6 @@
+"""
+Rend le dossier `bot/` importable comme package Python.
+
+Sans ce fichier, selon le contexte d'exécution, `python -m bot` et
+les imports de type `bot.admin` peuvent devenir fragiles.
+"""
